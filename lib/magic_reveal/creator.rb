58fb9fc4b62530dba898582483b117691d0235e9
Christian Höltje <docwhat@gerf.org>
2013-08-14 00:05:41 -0400

    Added Gemfile for 'new'

--- lib/magic_reveal/creator.rb
+++ lib/magic_reveal/creator.rb
@@ -27,12 +27,22 @@ module MagicReveal
     def create_project(project)
       top_dir = directory + project
       reveal_dir = top_dir + 'reveal.js'
+      gemfile = top_dir + 'Gemfile'
 
       top_dir.mkdir
 
       reveal_js_fetcher.save_to(reveal_dir)
       FileUtils.copy_file template_slides.to_s, (top_dir + 'slides.md').to_s
       FileUtils.copy_file template_config_ru.to_s, (top_dir + 'config.ru').to_s
+      gemfile.open('w') do |f|
+        f.puts "source 'https://rubygems.org'"
+        f.puts
+        f.puts "gem 'magic-reveal', '~> #{VERSION}'"
+      end
+
+      Dir.chdir(top_dir.to_s) do
+        system 'bundle install'
+      end
     end
   end
 end
