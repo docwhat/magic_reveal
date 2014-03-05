13a17d24009695895e29b87c3de32347b86efb31
Christian Höltje <docwhat@gerf.org>
2014-03-04 21:19:30 -0500

    Fixed coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,22 @@
+require 'simplecov'
+require 'coveralls'
+
+if ENV['TRAVIS'] == 'true'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    Coveralls::SimpleCov::Formatter,
+    SimpleCov::Formatter::HTMLFormatter
+  ]
+end
+
+SimpleCov.command_name 'RSpec'
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pathname'
 require 'faker'
 
 I18n.enforce_available_locales = true if I18n.respond_to? :enforce_available_locales
 EXAMPLE_DATA = Pathname.new(__FILE__).dirname + 'data'
-
-if ENV['TRAVIS'] == 'true'
-  require 'coveralls'
-  Coveralls.wear!
-else
-  begin
-    require 'simplecov'
-    SimpleCov.start
-  rescue LoadError
-    warn 'Unable to load simplecov; skipping coverage report'
-  end
-end
 
 RSpec.configure do |c|
   c.run_all_when_everything_filtered = true
